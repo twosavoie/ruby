fe0bb67dfa3f7cf43dbb7b4ecc6386b088beeeb6
lsavoie <twosavoie@gmail.com>
2018-08-09 12:54:03 -0500

    update greeter

--- ruby_challenges/irc/irc_1.rb
+++ ruby_challenges/irc/irc_1.rb
@@ -1,25 +1,30 @@
 #https://learn.skillcrush.com/module-20/ping-pong/
-#I don't know if this branch thing is working or not
+#ok making a change...
+#see this is a different change
+
 require 'socket'
 
-server = 'irc.freenode.net'
+server = 'moon.freenode.net'
 port = 6667
 socket = TCPSocket.open(server, port)
 
 nickname = 'SkillcrushBotOMG'
+channel = '#twos22'
 
 socket.puts "NICK #{nickname}"
 socket.puts "USER #{nickname} O * #{nickname}"
+socket.puts "JOIN #{channel}"
+socket.puts "PRIVMSG #{channel} :Really?!" #irc response when I run script
 
 while message = socket.gets do
   puts message
 
   if message.match('^PING :')
-
     server = message.split(':').last
-
     puts "PONG #{server}"
-
     socket.puts "PONG #{server}"
+  elsif message.match('Does this work?') #this has to match what I type into freenode.net
+    puts "PRIVMSG #{channel} :This Works!"
+    socket.puts "PRIVMSG #{channel} :This Works!" #responce to message.match
   end
 end
